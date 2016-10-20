00fc3872dc78c55fe6d2075bdd49d46b046731b6
Sam <Sam>
2016-10-19 20:53:43 -0500

    temp

--- cruddy2/__init__.py
+++ cruddy2/__init__.py
@@ -3,6 +3,7 @@ import psycopg2
 import urlparse
 from flask import Flask, render_template, request, redirect, url_for
 from model.User import User
+from model.Thing import Thing
 from model.DBSessionManager import DBSessionManager
 
 app = Flask(__name__)
@@ -15,6 +16,14 @@ def index():
     sessionManager.CommitToSession(user)
     return render_template('index.html')
 
+@app.route('/creatething')
+def createThing():
+    # sessionManager = DBSessionManager()
+    # thing = Thing('Car')
+    # return thing.thingname
+    # return thing.UserId
+    # sessionManager.CommitToSession(thing)
+    return render_template('creatething.html')
 
 @app.route('/databaseurl')
 def dburl():
--- cruddy2/model/Thing.py
+++ cruddy2/model/Thing.py
@@ -1,5 +1,17 @@
+from sqlalchemy import Column, Integer, String, Sequence, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
 class Thing():
     # Car
 
+    __tablename__ = 'things'
+
+    thingid = Column(Integer, Sequence('things_thingid_seq'), primary_key=True)
+    thingname = Column(String)
+    UserId = Column('userid', Integer, ForeignKey('user.userid'))
+
     def __init__(self, name):
-        self.Name = name
+        self.thingname = name
+        self.UserId = 2

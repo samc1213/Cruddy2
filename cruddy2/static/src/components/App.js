165e0ff0533f04efcb20c5b4ab4a904949ea7302
edwinwu16 <edwinwu16>
2016-12-21 21:27:29 -0600

    ViewCraiglist and (Post New Thing Instace) has link to post new thinginstance in the top right. Fixes #38.

--- cruddy2/static/src/components/App.js
+++ cruddy2/static/src/components/App.js
@@ -10,6 +10,11 @@ class App extends React.Component {
 	}
   render(){
   	console.log(this.props.loggedInUser);
+  	if ((this.props.params.websiteName != null) && (this.props.params.thingId !=null))
+  	{
+  		var returnstring = '/'+String(this.props.params.websiteName)+'/createthinginstance/'+String(this.props.params.thingId)
+  		var newinstancebutton = <li style={{float:'right', paddingTop:'5px', paddingBottom:'5px'}}> <a href={returnstring}> New Thing Instance </a> </li> 
+  	}
   	if (this.props.loggedInUser != null)
   	{
   		var username = <p className="dropdown-item">Logged in as {this.props.loggedInUser}</p>;
@@ -20,7 +25,6 @@ class App extends React.Component {
   	{
   		var login = <a className="dropdown-item" href="/login">Login</a>;
   	}
-
    	if (['/creatething', '/login', '/dashboard', '/'].indexOf(this.props.location.pathname) >= 0)
   	{
   		var nav = <li className="nav-item dropdown float-*-right">
@@ -36,12 +40,17 @@ class App extends React.Component {
     return (
     	<div>
 	    <nav className="navbar navbar-light bg-faded">
+	      <div className="container-fluid">
 	      <div className="navbar-header">
 	        <a className="navbar-brand" href="/">Cruddy2</a>
 	      </div>
 	      <ul className="nav navbar-nav float-*-right">
-	      {nav}
-    		</ul>
+	        {nav}
+	      </ul>
+	      <ul className="nav navbar-nav navbar-right">
+	        {newinstancebutton}
+	      </ul>
+	      </div>
 	    </nav>
 	  <div>  
 	    {this.props.children}

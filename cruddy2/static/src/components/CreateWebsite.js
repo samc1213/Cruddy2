2e8ae301e25cf5c87b5bcf953f1dc5506d19e3f6
Sam Cohen <samc1213@gmail.com>
2016-12-25 20:31:58 -0500

    new step for naming website, then picker. closes #30

--- cruddy2/static/src/components/CreateWebsite.js
+++ cruddy2/static/src/components/CreateWebsite.js
@@ -5,6 +5,34 @@ import CraigslistCardPreview from './CraigslistCardPreview'
 class CreateWebsite extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {websiteNameVisibility: 'block', websiteTypeVisibility: 'none', websiteName: ''}
+
+    this.handleWebsiteNameSubmit = this.handleWebsiteNameSubmit.bind(this);
+    this.handleWebsiteNameChange = this.handleWebsiteNameChange.bind(this);
+    this.handleWebsiteNameKeyDown = this.handleWebsiteNameKeyDown.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleWebsiteNameKeyDown(event) {
+    console.log(event.keyCode);
+    if (event.keyCode == 13) {
+      this.setState({websiteNameVisibility: 'none', websiteTypeVisibility:'block'});
+    }
+  }
+
+  handleWebsiteNameSubmit(event) {
+    event.preventDefault();
+    console.log("cock");
+    this.setState({websiteNameVisibility: 'none', websiteTypeVisibility:'block'});
+  }
+
+  handleWebsiteNameChange(event) {
+    this.setState({websiteName: event.target.value});
   }
 
   render(){
@@ -14,18 +42,20 @@ class CreateWebsite extends React.Component {
         <div>
         <h2 className="text-xs-center">Create Your Website</h2>
 
-          <form action="/api/postnewwebsite" method="POST" encType="multipart/form-data" style={{textAlign:'center'}}>
-            <label> Website Name </label>
-            <input type="text" name="websitename"/>
+          <form action="/api/postnewwebsite" method="POST" encType="multipart/form-data" style={{textAlign:'center'}} onSubmit={this.handleFormSubmit}>
+            <div style={{display: this.state.websiteNameVisibility}}>
+              <label> Website Name </label>
+              <input type="text" name="websitename" value={this.state.websiteName} onChange={this.handleWebsiteNameChange} onKeyDown={this.handleWebsiteNameKeyDown}/>
+              <p style={{marginBottom: '4px'}}> Your website will be available at: <br />
+                {`http://www.cruddy2.herokuapp.com/${this.state.websiteName}`}</p>
+              <a className="btn btn-primary" onClick={this.handleWebsiteNameSubmit}> Submit </a>
+            </div>
+
             <input type="hidden" name="username" value = {this.props.loggedInUser} />
             <input type="hidden" name="websitetypeid" value="0" />
 
+          <div is id="myCarousel" class="carousel slide" style={{width: '30%', margin:'auto', display: this.state.websiteTypeVisibility}} data-interval="false">
             <h4 className="text-xs-center"> What would you like your website to look like? </h4>
-          <div id="myCarousel" className="carousel slide" style={{width: '30%', margin:'auto'}}>
-            <ol className="carousel-indicators">
-              <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-              <li data-target="#myCarousel" data-slide-to="1"></li>
-            </ol>
 
             <div className="carousel-inner" role="listbox">
               <div className="carousel-item active">
@@ -44,10 +74,8 @@ class CreateWebsite extends React.Component {
             <a className="right carousel-control" style={{right:'-15%', backgroundImage:'none'}} href="#myCarousel" role="button" data-slide="next">
               <span style={{color: 'black', position:'absolute', top:'45%', height:'10%', right:'-3px'}}><i className="fa fa-caret-square-o-right" aria-hidden="true"></i></span>
             </a>
-
-            <button style={{position:'absolute', top:'175px', left:'35%', width:'30%'}} className="btn btn-primary" type="submit" > Submit </button>
+            <button style={{position:'absolute', top:'300px', left:'35%', width:'30%'}} className="btn btn-primary" type="submit"> Submit </button>
           </div>
-
           </form>
         </div>
     );
